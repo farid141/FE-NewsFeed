364b6d7ee8be773502a40005cb09642323636b9f
farid nubaili <farid.nubaili@tritronik.com>
2025-11-10 17:02:01 +0700

    fix: modify format time helper to add seconds

--- helper/index.ts
+++ helper/index.ts
@@ -1,13 +1,17 @@
 export const formatTime = (timestamp: any) => {
-    if(!timestamp) return '-';
+  if (!timestamp) return "-";
 
-    const date = new Date(timestamp);
-    const diff = Date.now() - date.getTime();
-    const minutes = Math.floor(diff / 60000);
-    const hours = Math.floor(diff / 3600000);
-    const days = Math.floor(diff / 86400000);
-    
-    if (minutes < 60) return `${minutes}m ago`;
-    if (hours < 24) return `${hours}h ago`;
-    return `${days}d ago`;
-  };
+  const date = new Date(timestamp);
+  const now = new Date();
+
+  const diff = now.getTime() - date.getTime(); // ✅ type-safe
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(diff / 60000);
+  const hours = Math.floor(diff / 3600000);
+  const days = Math.floor(diff / 86400000);
+
+  if (seconds < 60) return `${seconds}s ago`;
+  else if (minutes < 60) return `${minutes}m ago`;
+  else if (hours < 24) return `${hours}h ago`;
+  else `${days}d ago`;
+};
